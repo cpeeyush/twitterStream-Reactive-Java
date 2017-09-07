c34a9f701f9586c52b8a2c6c3fc8176b0b030249
Peeyush Chandel <peeyush.chandel@trivago.com>
2017-09-07 11:50:20 +0200

    First Commit

---
+++ src/main/java/com/peeyush/App.java
@@ -0,0 +1,13 @@
+package com.peeyush;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
